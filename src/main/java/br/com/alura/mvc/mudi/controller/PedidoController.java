94b1abfff27f38a68547db1ad05c0b977b911012
Francisco Berti <berti.francisco@gmail.com>
2021-02-24 17:54:59 -0300

    validacao do pedido

--- src/main/java/br/com/alura/mvc/mudi/controller/PedidoController.java
+++ src/main/java/br/com/alura/mvc/mudi/controller/PedidoController.java
@@ -1,15 +1,38 @@
 package br.com.alura.mvc.mudi.controller;
 
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.bind.BindResult;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import br.com.alura.mvc.mudi.dto.RequisicaoNovoPedido;
+import br.com.alura.mvc.mudi.model.Pedido;
+import br.com.alura.mvc.mudi.repository.PedidoRepository;
 
 @Controller
 @RequestMapping("pedido") //todas as requisições para pedido irão bater nesse Controller aqui
 public class PedidoController {
 	
+	@Autowired
+	private PedidoRepository pedidoRepository;
+	
 	@GetMapping("formulario")
 	public String formulario() { //pagina que deve ser aberta
+		return "pedido/formulario";
+	}
+	
+	@PostMapping("novo")
+	public String novo(@Valid RequisicaoNovoPedido requisicao, BindingResult result) {
+		if(result.hasErrors()) {
+			return "pedido/formulario";
+		}
+		Pedido pedido = requisicao.toPedido();
+		pedidoRepository.save(pedido);
 		return "pedido/formulario";
 	}
 
